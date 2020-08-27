4e07464f7b0fab022683e2d06951024ca3f336b3
joserobertopinto <pintojose86@gmail.com>
2020-08-26 22:38:20 -0300

    configuro coneccion a db

--- config/db.php
+++ config/db.php
@@ -1,11 +1,17 @@
 <?php
+$postgres_ip    = 'localhost';
+$postgres_db    = 'ordenes_trabajo';
+$postgres_user  = 'demo';
+$postgres_pwd   = 'demo123';
+$enableCache    = true;
+$postgres_port  = '5432';
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'     => 'yii\db\Connection',
+    'dsn'       => "pgsql:host=$postgres_ip;port=$postgres_port;dbname=$postgres_db",
+    'username'  => $postgres_user,
+    'password'  => $postgres_pwd,
+    'charset'   => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
