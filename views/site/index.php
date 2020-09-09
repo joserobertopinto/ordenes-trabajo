c987092e72db227ff0a987eb0d5dea962152132a
joserobertopinto <pintojose86@gmail.com>
2020-09-09 01:21:42 -0300

    dashboad parcial

--- views/site/index.php
+++ views/site/index.php
@@ -1,6 +1,9 @@
 <?php
 
 use rce\material\widgets\Card;
+use kartik\grid\GridView;
+use app\common\utils\Fecha;
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'Ordenes de Trabajo';
@@ -9,14 +12,14 @@ $this->title = 'Ordenes de Trabajo';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="col-lg-5 col-md-5 col-sm-5">
                 <?php Card::begin([
                     'header'=>'header-icon',
                     'type'=>'card-stats',
-                    'icon'=>'<i class="material-icons">content_copy</i>',
-                    'color'=>'warning',
-                    'title'=>'49/50<small>GB</small>',
-                    'subtitle'=>'Used Space',
+                    'icon'=>'<i class="material-icons">assignment_late</i>',
+                    'color'=>'danger',
+                    'title'=> $asignadasSinComenzar,
+                    'subtitle'=>'Ordenes asignadas con fecha de comienzo',
                     'footer'=>'<div class="stats">
                             <i class="material-icons text-danger">warning</i>
                             <a href="#">Get More Space...</a>
@@ -49,33 +52,154 @@ $this->title = 'Ordenes de Trabajo';
                           </div>',
                 ]); Card::end(); ?>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <?php Card::begin([
-                    'type'=>'card-stats',
-                    'header'=>'header-icon',
-                    'icon'=>'<i class="fa fa-twitter"></i>',
-                    'color'=>'info',
-                    'title'=>'+245',
-                    'subtitle'=>'Followers',
-                    'footer'=>'<div class="stats">
-                            <i class="material-icons">update</i> Just Updated
-                          </div>',
-                ]); Card::end(); ?>
-            </div>
-        </div>
+        </div> <!--END ROW -->
 
         <div class="row">
-            <div class="col-lg-4">
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
+            <div class="col-lg-6 col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-warning">
+                        <h4 class="card-title">
+                        <i class="fa fa-exclamation-circle" aria-hidden="true"></i>&nbsp;Tareas pendientes asignadas Recientemente</h4>
+                        <p class="card-category">New employees on 15th September, 2016</p>
+                    </div>
+                    <div class="card-body table-responsive">
+                        <!-- GRILLA KARTIK -->
+                        <?=
+                            GridView::widget([
+                                'dataProvider'=> $dataProviderAsignadas,
+                                'filterModel' => false,
+                                'summary' => '',
+                                'striped' => false,
+                                'bordered'=>false,
+                                'options' => [
+                                    'class' => 'gridClassWarning',
+                                 ],
+                                'columns' => [
+                                    [
+                                        'attribute' => 'id_ordenes_trabajo',
+                                        'visible'   => false,
+                                    ],
+                                    [
+                                        'attribute' => 'nro_orden_trabajo',
+                                        'label'     => 'Nro.',
+                                        'visible'   => true,
+                                        'headerOptions' => ['style' => 'width:10%'],
+                                    ],
+                                    'titulo',
+                                    [
+                                        'attribute' => 'fecha_hora_comienzo',
+                                        'label'		=> 'Comienzo',
+                                        'value'		=> function ($model) {
+                                                return Fecha::convertir($model->fecha_hora_comienzo);
+                                            },
+                                        'filter' => false,
+                                    ],
+                                    [
+                                        'class' => 'kartik\grid\ActionColumn',
+                                        'headerOptions' => ['id' => 'headerGrilla'],
+                                        'dropdown' => false,
+                                        'template' => '{view}', // .' {custom}',
+                                        'buttons' => [
+                                            'view' => function ($url, $model) {
+                                                return Html::a( '<i class="material-icons">visibility</i>',
+                                                    [Yii::$app->urlManager->createUrl(['view', 'id' => $model->id_ordenes_trabajo])],
+                                                    ['title' => 'ver']
+                                                );
+                                            },
+                                        ],
+                                    ],
+                                ],
+                                'pjax'=>false,
+                            ]);
+                        ?>
+                    </div>
+                </div>
+            </div><!-- END CARD ASIGNADAS RECIENTEMENTE -->
 
-    </div>
+            <div class="col-lg-6 col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-success">
+                        <h4 class="card-title">
+                        <i class="fa fa-check-square-o"></i>&nbsp;Tareas finalizadas Recientemente
+                        </h4>
+                        <p class="card-category">New employees on 15th September, 2016</p>
+                    </div>
+                    <div class="card-body table-responsive">
+                        <!-- GRILLA KARTIK -->
+                        <?=
+                            GridView::widget([
+                                'dataProvider'=> $dataProviderFinalizadas,
+                                'filterModel' => false,
+                                'summary' => '',
+                                'striped' => false,
+                                'bordered'=>false,
+                                'options' => [
+                                    'class' => 'gridClassSuccess',
+                                 ],
+                                'columns' => [
+                                    [
+                                        'attribute' => 'id_ordenes_trabajo',
+                                        'visible'   => false,
+                                    ],
+                                    [
+                                        'attribute' => 'nro_orden_trabajo',
+                                        'label'     => 'Nro.',
+                                        'visible'   => true,
+                                        'headerOptions' => ['style' => 'width:10%'],
+                                    ],
+                                    'titulo',
+                                    [
+                                        'attribute' => 'fecha_hora_finalizacion',
+                                        'label'		=> 'Finalizada',
+                                        'value'		=> function ($model) {
+                                                return Fecha::convertir($model->fecha_hora_finalizacion);
+                                            },
+                                        'filter' => false,
+                                    ],
+                                    [
+                                        'class' => 'kartik\grid\ActionColumn',
+                                        'headerOptions' => ['id' => 'headerGrilla'],
+                                        'dropdown' => false,
+                                        'template' => '{view}', // .' {custom}',
+                                        'buttons' => [
+                                            'view' => function ($url, $model) {
+                                                return Html::a( '<i class="material-icons">visibility</i>',
+                                                    [Yii::$app->urlManager->createUrl(['view', 'id' => $model->id_ordenes_trabajo])],
+                                                    ['title' => 'ver']
+                                                );
+                                            },
+                                        ],
+                                    ],
+                                ],
+                                'pjax'=>false,
+                            ]);
+                        ?>
+                    </div>
+                </div>
+            </div><!-- END CARD FINALIZADAS RECIENTEMENTE -->
+        </div> <!-- END ROW CARD GRILLAS-->
 
-</div>
+    </div><!-- END BODY CONTENT -->
+
+</div><!-- END SITE INDEX -->
+
+<?php 
+    $this->registerJs("
+        $(document).ready(function(){
+            //fix rowpan de gridview actions para bs4
+            $('#headerGrilla').attr('rowspan','1');
+        });    
+    ");
+    $this->registerCss("
+        /** fix rowpan de gridview actions para bs4 **/
+        #headerGrilla, #w0-filters, #w4-filters{
+            display : none!important;
+        }
+        .gridClassWarning table thead tr th a{
+            color: #fb8c00;
+        }
+        .gridClassSuccess table thead tr th a{
+            color: #43a047;
+        }
+    ");
+?>
